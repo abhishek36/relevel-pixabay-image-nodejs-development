21487ef8645422e0eca32a1eb7f663c14a95f512
abhishek36 <akamble9913@gmail.com>
2022-01-17 11:42:52 +0530

    first commit

---
+++ src/controllers/images.controller.js
@@ -0,0 +1,35 @@
+//import Image model here
+
+const addImage = async (req, res) => {
+  //addImage api logic here
+};
+
+const getAllImage = async (req, res) => {
+  //getAllImage api logic here
+};
+
+const getImageByCategoryId = async (req, res) => {
+  //getImageByCategoryId api logic here
+};
+
+const getImageById = async (req, res) => {
+  //getImageById api logic here
+};
+
+const updateImageById = async (req, res) => {
+  //updateImageById api logic here
+};
+
+const deleteImageById = async (req, res) => {
+  //deleteImageById api logic here
+};
+
+const ImageController = {
+  addImage,
+  getAllImage,
+  getImageByCategoryId,
+  getImageById,
+  updateImageById,
+  deleteImageById,
+};
+module.exports = ImageController;
